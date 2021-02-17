1b83d0022c388f775d8cbca98477b20c0135bab6
Mateusz Zając <matzayonc@gmail.com>
2021-02-17 17:26:45 +0100

    shortener status style

--- src/routes/Shortener.svelte
+++ src/routes/Shortener.svelte
@@ -1,19 +1,40 @@
 <script lang='ts'>
     export let location = undefined
-    location = location
 
+    location = location
+    
+    interface Response {success?: boolean, err?:string}
 
     let link = ''
     let alias = ''
+    
+    let ok:boolean|undefined = undefined
 
-    function submit(){
-        console.log(link, alias)
+    async function submit(){
+
+        const res = await fetch(`/addshorten/${alias}`, {
+            method: 'POST',
+            body: JSON.stringify({link: link})
+        }).then(r => r.json()) as Response
+
+        console.log(res)
+        ok = res.success
+
     }
+
 
 </script>
 
 
 <h1>Shorten ur links</h1>
+
+{#if ok}
+    <p>Shortened link saved</p>
+{:else if ok === false}
+    <p class='err'>Error: probably name is taken</p>
+{:else if alias && !link.match(/https?:\/\//)}
+    <p class='err'>invalid link</p>
+{/if}
 
 <input bind:value={link} placeholder='link'>
 <input bind:value={alias} placeholder='alias'>
@@ -26,5 +47,15 @@
         font-size: 18px
         margin: 10px 50px
         display: block
+
+    p
+        margin: 10px 50px
+        background-color: lightgrey
+        display: inline-block
+        padding: 10px 20px
+        border-radius: 20px
+
+        &.err
+            color: red
     
 </style>
